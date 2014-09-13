0b431178c166354e6af747cd995ef935cc5e3bc5
Teloo <teru1721@gmail.com>
2014-09-13 15:34:48 +0900

    Display deleted event count and fix error message bug

--- example/main.js
+++ example/main.js
@@ -4,4 +4,9 @@ delevtsSalesforce.deleteEvents({
   username: '<Salesforce account username>',
   password: '<Salesforce account password>',
   securityToken: '<Salesforce account security token>'
+}, function(err, count) {
+  if (err) {
+    throw err;
+  }
+  console.log('Count: ' + count);
 });
